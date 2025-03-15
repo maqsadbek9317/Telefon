c6189ac41630251c62228efa96dca9ff56dce655
maqsadbek9317 <maqsadbekqurdashev@gmail.com>
2025-03-15 11:33:39 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Bu yerda JavaScript kodlarini qo'shishingiz mumkin, masalan, sahifada interaktiv elementlar
+document.addEventListener("DOMContentLoaded", function() {
+    // Misol uchun, telefon haqida qo'shimcha ma'lumotlarni chiqarish
+    console.log("Redmi Note 13 sahifasi yuklandi");
+});
+<button onclick="alert('Siz buyurtma berdingiz!')">Buyurtma berish</button>
